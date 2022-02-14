33a7b8af7127c9f72512764ff53c352299ffcecf
Miki Blasco <blascom@7@gmail.com>
2022-02-14 17:35:31 +0100

    lesson done

--- config/index.js
+++ config/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
+const session = require("express-session")
 const favicon = require("serve-favicon");
+const MongoStore = require("connect-mongo")
 const path = require("path");
 
 module.exports = (app) => {
   app.use(logger("dev"));
   app.use(express.json());
-  app.use(express.urlencoded({ ex
+  app.use(express.urlencoded({ extended: true }))
+  app.use(cookieParser())
+
+  app.use(
+    session({
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        maxAge: 24 * 60 * 60 * 1000, //The cookie is one day old as maximum now. Will last 24h
+      },
+        saveUninitialized: false,
+        resave: false,
+        store: MongoStore.create({
+          mongoUrl: process.env.MONGODB_URI,
+          ttl: 24 * 60 * 60,
+        })
+      }
+  ));
+
+
   app.set("views", path.join(__dirname, "..", "views"));
   app.set("view engine", "hbs");
   app.use(express.static(path.join(__dirname, "..", "public")));
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,19 +1,89 @@
 const express = require('express');
 const User = require('../models/User.model');
 const router = express.Router();
-const saltRound = 10;
+const saltRounds = 5;
 const bcrypt = require('bcrypt');
 
-router.get('/signup', (req, res, next) => {
+
+
+router.route('/signup')
+.get( (rq, res) => {
 	res.render('signup');
+})
+
+.post((req, res) =>{
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if(!username || !password) {
+		res.render("signup", {errorMessage: "All fields are required"});
+	}
+
+	User.findOne({ username }).then((user)=> {
+		if (user && user.username) {
+			res.render("signup", { errorMessage: "User already taken"})
+			
+		}
+			const salt = bcrypt.genSaltSync(saltRounds);
+			const hashedPwd = bcrypt.hashSync(password, salt);  
+		    User.create({ username, password: hashedPwd}).then (()=> res.redirect("/"))
+			    
+   });
 });
 
-router.get('/login', (req, res, next) => {
-	res.render('login');
-});
 
+
+router
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+	console.log("HOLA")
+
+    if (!username || !password) {
+      res.render("login", { errorMessage: "All filds are required" });
+      throw new Error("Validation error");
+    }
+
+    User.findOne({ username })
+      .then((user) => {
+        if (!user) {
+          res.render("login", { errorMessage: "Incorrect credentials!" });
+          throw new Error("Validation error");
+        }
+
+        const isPwCorrect = bcrypt.compareSync(password, user.password);
+        
+        if (isPwCorrect) {
+          //loading the cookie
+          req.session.currentUserId = user._id
+          //destroy de memory redirecting?
+          res.redirect("/auth/profile");
+        } else {
+          res.render("login", { errorMessage: "Incorrect credentials!" });
+        }
+      })
+      .catch((error) => console.log(error));
+  });
+
+
+// you com on here with a completely clean new request
 router.get('/profile', (req, res) => {
-	res.render('profile');
+  const id = req.session.currentUserId;
+  User.findById(id)
+  .then((user)=> res.render("profile", user))
+  .catch(err=>console.log(err));
+
 });
+
+router.get("/logout", (req,res)=>{
+  req.session.destroy((err)=>{
+    console.log("a tu casa")
+    res.redirect("/")
+  }
+)})
 
 module.exports = router;
